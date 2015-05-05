0edf392d424adaa79c992ba0110bd39ea2bc849d
Emilio Palumbo <emiliopalumbo@gmail.com>
2015-05-05 17:16:39 +0200

    Update encodeEvaluation2ga.py to download tarball as tempfile

--- encodeEvaluation2ga.py
+++ encodeEvaluation2ga.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from __future__ import division
 
 import os
@@ -5,66 +6,23 @@ import sys
 import errno
 import string
 import tarfile
-
-def main(argv):
-
-    description = "RNAseq data from ENCODE evaluation"
-    annotationId = "Gencodev16"
-
-    #tar = tarfile.open('ENCODE-benchmark-data.tgz', 'r')
-    tar = tarfile.open(argv[1], 'r')
-
-    write_rnaseq_tables(tar, description, annotationId)
-    write_counts_tables(tar)
-    #write_dist_tables(tar)
-    write_expression_tables(tar, annotationId)
-
-    tar.close()
-
-def write_rnaseq_tables(tar, description, annotationId):
-    for analysisId in get_analysis_ids_from_tar(tar):
-        # create analysis id folder
-        make_dir(analysisId)
-
-        # output table
-        rnaSeqTable = os.path.join(analysisId, "rnaseq.table")
-
-        # write rnaseq table
-        with open(rnaSeqTable, "w") as rnaQuantFile:
-            writeRNAQuant(rnaQuantFile, analysisId, description, annotationId)
+import requests
+import tempfile
 
 
-def write_counts_tables(tar):
-    for analysisId, samstatsfile in get_members_from_tar(tar, 'Log.final.out'):
-        # output table  
-        countsTable = os.path.join(analysisId, "counts.table")
-
-        samstats = getSamstats(samstatsfile)
-        
-        # write mapping stats table
-        with open(countsTable, "w") as samOutfile:
-            writeSamstats(samOutfile, samstats, analysisId)
+def log(message):
+    print >>sys.stderr, "== {}".format(message)
 
 
-def write_dist_tables(tar):
-    for analysisId, distfile in get_members_from_tar(tar, 'genome_cov'):
-        # output table 
-        distTable = os.path.join(analysisId, "dist.table")
-        
-        # write mapping distribution table
-        distribution = getDistribution(distfile)
-        with open(distTable, "w") as distOutfile:
-            writeDistribution(distOutfile, distribution, analysisId, samstats["mapped"])
-
-
-def write_expression_tables(tar, annotationId):
-    for analysisId, quantfile in get_members_from_tar(tar, 'RSEM.gene'):
-        # output table
-        expTable = os.path.join(analysisId, "expression.table")
-        
-        # write expression table
-        with open(expTable, "w") as quantOutfile:
-            write_expression(analysisId, annotationId, quantfile, quantOutfile)
+def get_tarball(tarball_url):
+    tmp_tarball = tempfile.NamedTemporaryFile(mode='w+b')
+    r = requests.get(tarball_url, stream=True)
+    log('Downloading tarball from {0}'.format(tarball_url))
+    for chunk in r.iter_content(chunk_size=1024): 
+        if chunk: # filter out keep-alive new chunks
+            tmp_tarball.write(chunk)
+            tmp_tarball.flush()
+    return tmp_tarball
 
 
 def make_dir(path):
@@ -130,7 +88,7 @@ def getSamstats(file):
             content["unique"] = value
         elif key in ['Number of reads mapped to multiple loci', 'Number of reads mapped to too many loci']:
             content["multi"] = content.get('multi', 0) + int(value)
-        # STAR does not report unique and multi split-maps, only total
+        # TODO: STAR does not report unique and multi split-maps, only total
         elif key == 'Number of splices: Total':   
             content["msplice"] = content["usplice"] = value
 
@@ -174,6 +132,78 @@ def write_expression(analysisId, annotationId, quantfile, quantOutfile, tool='RS
         score = (float(fields[10]) + float(fields[11]))/2
         outline = string.join([expressionId, annotationId, expressionLevel, featureGroupId, isNormalized, rawCount, str(score), units], "\t")
         quantOutfile.write("%s\n" % outline)
+
+
+def write_rnaseq_tables(tar, description, annotationId):
+    log("Writing rnaseq tables")
+    for analysisId in get_analysis_ids_from_tar(tar):
+        # create analysis id folder
+        make_dir(analysisId)
+
+        # output table
+        rnaSeqTable = os.path.join(analysisId, "rnaseq.table")
+
+        # write rnaseq table
+        with open(rnaSeqTable, "w") as rnaQuantFile:
+            writeRNAQuant(rnaQuantFile, analysisId, description, annotationId)
+
+
+def write_counts_tables(tar):
+    log("Writing counts tables")
+    for analysisId, samstatsfile in get_members_from_tar(tar, 'Log.final.out'):
+        # output table  
+        countsTable = os.path.join(analysisId, "counts.table")
+
+        samstats = getSamstats(samstatsfile)
+        
+        # write mapping stats table
+        with open(countsTable, "w") as samOutfile:
+            writeSamstats(samOutfile, samstats, analysisId)
+
+
+def write_dist_tables(tar):
+    log("Writing distribution tables")
+    for analysisId, distfile in get_members_from_tar(tar, 'genome_cov'):
+        # output table 
+        distTable = os.path.join(analysisId, "dist.table")
+        
+        # write mapping distribution table
+        distribution = getDistribution(distfile)
+        with open(distTable, "w") as distOutfile:
+            writeDistribution(distOutfile, distribution, analysisId, samstats["mapped"])
+
+
+def write_expression_tables(tar, annotationId):
+    log("Writing gene expression tables")
+    for analysisId, quantfile in get_members_from_tar(tar, 'RSEM.gene'):
+        # output table
+        expTable = os.path.join(analysisId, "expression.table")
+        
+        # write expression table
+        with open(expTable, "w") as quantOutfile:
+            write_expression(analysisId, annotationId, quantfile, quantOutfile)
+
+
+def main(argv):
+
+    description = "RNAseq data from ENCODE evaluation"
+    annotationId = "Gencodev16"
+
+    tarball_url = 'http://genome.crg.es/~epalumbo/ENCODE-benchmark-data.tgz'
+    tarball = get_tarball(tarball_url)
+    
+    tar = tarfile.open(tarball.name, mode='r')
+
+    write_rnaseq_tables(tar, description, annotationId)
+    write_counts_tables(tar)
+    #TODO: get read distribution data
+    #write_dist_tables(tar)
+    write_expression_tables(tar, annotationId)
+
+    tarball.close()
+    tar.close()
+
+    log("DONE")
 
 
 if __name__ == '__main__':
